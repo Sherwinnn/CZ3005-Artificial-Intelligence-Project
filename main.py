a61dbc2ad1a63e5c77688a855ce05c4aef7eb28b
Sherwinnn <sherwinsamson1@hotmail.com>
2022-10-06 19:18:52 +0800

    Input initialization

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello")
+import json
+
+
+# 4 Dictionaries needed
+G = {}
+Dist = {}
+Cost = {}
+Coord = {}
+
+if __name__ == '__main__':
+    with open('G.json') as f:
+        G = json.load(f)
+    with open('Dist.json') as f:
+        Dist = json.load(f)
+    with open('Cost.json') as f:
+        Cost = json.load(f)
+    with open('Coord.json') as f:
+        Coord = json.load(f)
+
+    start = '1'
+    end = '50'
+    budget = 287932
+
+
+
+
