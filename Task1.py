21fba8a1b231e23bfe61fceefe76c9f7f1a4073e
Sherwinnn <sherwinsamson1@hotmail.com>
2022-10-06 20:07:36 +0800

    update t1

--- Task1.py
+++ Task1.py
@@ -1,15 +1,19 @@
+class Node:
+    def __init__(self, id, parent, dist, cost):
+        self.id = id
+        self.parent = parent  
+        self.dist = dist
+        self.cost = cost
+    # to sort nodes in pqueue based on total distance
+    def __lt__(self, next):
+        return self.dist < next.dist
 
-def dijkstra(graph, start, end):
-    return
-
-def makePath(start,end):
-    return
-
-def displayPath(start,end):
+def dijkstra(GDict, start, end):
     return
     
-def begin(Gdict, start, end):
-    path, cost, numvisited = dijkstra(Gdict,start,end)
-    shortestpath = makePath(path, end)
+
+
+def begin(GDict, start, end):
+    path, cost, numvisited = dijkstra(GDict,start,end)
 
     print()
