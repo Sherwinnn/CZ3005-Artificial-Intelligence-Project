9f0cc7acf65e38384f489803dbaa5111e2dc6c09
Sherwinnn <sherwinsamson1@hotmail.com>
2022-10-09 17:39:42 +0800

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,12 +1,9 @@
-import json
-import math
 from queue import PriorityQueue
 import sys
 import IOParser
 
 
 def dijkstra(GDict, DistDict, start, end):
-    
     pq = PriorityQueue()
     pq.put([0, [start]])
 
@@ -15,24 +12,24 @@ def dijkstra(GDict, DistDict, start, end):
     
     while pq:
         FirstNode = pq.get()
-        currentDist = FirstNode[0]
-        currentPath = FirstNode[1]
-        currentNode = currentPath[-1]
-        travelledDict[currentNode] = 1
+        currentDist = FirstNode[0]                      # Setting the accumulated distance thus far as currentDist
+        currentPath = FirstNode[1]                      # Obtaining the current path to be taken based on the dequeued node 
+        currentNode = currentPath[-1]                   # Setting the current node as last taken node in currentPath
+        travelledDict[currentNode] = 1                  # Setting values of current node in Travelled dict as 1 (visited)
 
-        if currentNode == end:
-            return currentPath, currentDist
+        if currentNode == end:                          # Check if goal is reached
+            return currentPath, currentDist             # Return the accumulated shortest path and distance thus far
 
-        for neighbour in GDict[currentNode]:
-            if neighbour not in distanceDict:
-                distanceDict[neighbour] = sys.maxsize
+        for adjNode in GDict[currentNode]:              # Parsing through each node connected to current node
+            if adjNode not in distanceDict:
+                distanceDict[adjNode] = sys.maxsize     # Setting maximum or infinite value if distance not stored in distanceDict yet
 
             newPath = currentPath[:]
-            newPath.append(neighbour)
-            newDistance = currentDist+ DistDict[min(currentNode,neighbour),max(currentNode,neighbour)]
+            newPath.append(adjNode)
+            newDistance = currentDist+ DistDict[min(currentNode,adjNode),max(currentNode,adjNode)]
 
-            if neighbour not in travelledDict and distanceDict[neighbour] > newDistance:
-                distanceDict[neighbour] = newDistance
+            if adjNode not in travelledDict and distanceDict[adjNode] > newDistance:    # Dijkstra algorithm's greedy approach to obtaining shortest path and distance
+                distanceDict[adjNode] = newDistance                                     
                 pq.put([newDistance, newPath])
                 
     
