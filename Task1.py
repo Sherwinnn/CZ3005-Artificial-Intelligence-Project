335dc9d44dcc2c828b89fa3439ef6023c0a71826
Sherwinnn <sherwinsamson1@hotmail.com>
2022-10-09 17:09:18 +0800

    update task 1

--- Task1.py
+++ Task1.py
@@ -1,22 +1,73 @@
+import json
+import math
+from queue import PriorityQueue
+import sys
 import IOParser
 
-class Node:
-    def __init__(self, id, parent, dist, cost):
-        self.id = id
-        self.parent = parent  
-        self.dist = dist
-        self.cost = cost
-    # to sort nodes in pqueue based on total distance
-    def __lt__(self, next):
-        return self.dist < next.dist
 
-def dijkstra(GDict, start, end):
-    return
+def dijkstra(GDict, DistDict, start, end):
+    
+    pq = PriorityQueue()
+
+    # push the starting index and path
+    pq.put([0, [start]])
+
+    # dictionary to keep track of visited node
+    travelled = {}
+
+    # dictionary to keep track of the node distance
+    # for checking if there's another shortest path
+    distanceDict = {start: sys.maxsize}
+    
+    # while the queue is not empty
+    while pq:
+
+        # pop the element with the highest priority
+        e = pq.get()
+
+        # get the current distance
+        currentDist = e[0]
+
+        # get the current path
+        currentPath = e[1]
+
+        # set the current node to the last node in the path
+        currentNode = currentPath[-1]
+
+        # mark current ndoe as travelled
+        travelled[currentNode] = 1
+
+        # check if current node is the endination node
+        if currentNode == end:
+            # return the path and the total distance from source to destination node
+            return currentPath, currentDist
+
+        # check for adjacent node
+        for neighbour in GDict[currentNode]:
+            
+            # if the distance for this not is not initialized
+            if neighbour not in distanceDict:
+                # initialized the node distance
+                distanceDict[neighbour] = sys.maxsize
+            
+            # clone current path to a new path to 
+            # prevent appending to the current path
+            newPath = currentPath[:]
+            # append the adjacent node to the new path
+            newPath.append(neighbour)
+
+            # calculate the new distance
+            newDistance = currentDist+ DistDict[min(currentNode,neighbour),max(currentNode,neighbour)]
+            ##newDist = cur_dist + dist[f"{cur_node},{neighbour}"]
+
+            # if adjacent node is not in visted and if the new distance smaller than the old distance
+            if neighbour not in travelled and distanceDict[neighbour] > newDistance:
+                # push adjacent node with it's distance and path into priority pq
+                pq.put([newDistance, newPath])
+                # update the distance of the node
+                distanceDict[neighbour] = newDistance
     
 
-
-def begin(GDict, start, end):
-    pass
-    # path, cost, numvisited = dijkstra(GDict,start,end)
-
-    # print()
+def begin(GDict, DistDict, start, end):
+    Path, Dist = dijkstra(GDict, DistDict, start, end)
+    IOParser.outputParser((Path, Dist, 0))
