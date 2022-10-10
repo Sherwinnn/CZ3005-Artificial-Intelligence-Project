59d4290c21538f32d4c014c4e8084e4e4631b963
RyanCodesHard <ryan.taw96@gmail.com>
2022-10-10 09:45:31 +0800

    added path

--- Task3.py
+++ Task3.py
@@ -17,32 +17,25 @@ def heuristic(CoordDict, end, child, distancet):
     return distancet+sqrt( (CoordDict[end][0]-CoordDict[child][0] )**2 + (CoordDict[end][1]- CoordDict[child][1])**2 )
 
 def aStar(GDict, DistDict, CostDict, CoordDict, start, end, budget):
-    travelled = {} #stores {node : distance from start to that node}
+    travelled = {} #stores {node : cost remaining from that node}
     openlist = PriorityQueue()
-    openlist.put( (0, (start,[start], 0, budget) ) ) #(fvalue, (node, path, gvalue, budgetRemaining) )
-    distance = 0
+    openlist.put( (0, (start,[start], 0, 0) ) ) #(fvalue, (node, path, gvalue, cost) )
+    distance = -1
 
     while not openlist.empty():
-        curnode,path,distFromStart,budgetRemaining = openlist.get()[1]
-        if travelled.get(curnode)!=None and budgetRemaining<travelled[curnode][1]:
+        curnode,path,distFromStart,cost = openlist.get()[1]
+        if travelled.get(curnode)!=None and cost>travelled[curnode][1]:
             continue
         
-        travelled[curnode] = (distFromStart,budgetRemaining)
+        travelled[curnode] = (distFromStart,cost)
         if curnode==end:
             distance = distFromStart
-            budget -= budgetRemaining
             path+=[curnode]
-            # pathnode = end
-            # while (pathnode!=start):
-            #     path.append(pathnode)
-            #     pathnode = travelled[pathnode][0]
-            # path = path[::-1]
             break
         for child in GDict[curnode]:
-            # print("child:",child)
             distStartToChild = distFromStart+DistDict[min(curnode,child),max(curnode,child)]
-            budgetAfterDeduct = budgetRemaining-CostDict[min(curnode,child),max(curnode,child)]
-            if budgetAfterDeduct<0:  #check if not enough budget
+            childcost= cost + CostDict[min(curnode,child),max(curnode,child)]
+            if childcost>budget:  #check if not enough budget
                 continue
             openlist.put(
                 (
@@ -51,10 +44,10 @@ def aStar(GDict, DistDict, CostDict, CoordDict, start, end, budget):
                     end, child, 
                     distStartToChild, 
                     ),
-                    (child,path+[curnode],distStartToChild, budgetAfterDeduct)
+                    (child,path+[curnode],distStartToChild, childcost)
                 )
             )
-    return path,distance,budget
+    return path,distance,cost
 
 def begin(GDict, DistDict, CostDict, CoordDict, start, end, budget):
     return IOParser.outputParser(aStar(GDict, DistDict, CostDict, CoordDict, start, end, budget))
